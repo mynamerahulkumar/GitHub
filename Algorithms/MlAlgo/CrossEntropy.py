7c789b04a7e316cebd4605d31db4be5ad866fb83
Rahul Kumar <mynamerahulkumar13@gmail.com>
2019-02-19 15:57:17 +0530

    Update CrossEntropy.py

--- Algorithms/MlAlgo/CrossEntropy.py
+++ Algorithms/MlAlgo/CrossEntropy.py
@@ -1,6 +1,6 @@
 import numpy as np
 import  math
-# Write a function that takes as input two lists Y, P,
+# Write a function that takes as input two lists  Y, P,
 # and returns the float corresponding to their cross-entropy.
 def cross_entropy(Y, P):
     ce=0
