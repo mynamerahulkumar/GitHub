0f0ff64d00ef0a2a45ea1796d48a24b6f27cac0a
Rahul Kumar <mynamerahulkumar13@gmail.com>
2019-08-23 10:13:52 +0530

    structured the files

--- python/numpy/np_reshape_problem.py
+++
@@ -1,7 +0,0 @@
-import numpy as np
-
-array_1 = np.array(list(map(int,input().split())))
-
-print(np.reshape(array_1,(3,3)))
-
-
--- python/numpy/numpy_sum_prod.py
+++
@@ -1,6 +0,0 @@
-import numpy as np
-
-n,m=map(int,input().split())
-arr1=np.array([input().split() for _ in range(n) ],dtype=int)
-sum1=np.sum(arr1,axis=0)
-print(np.product(sum1,axis=0))
