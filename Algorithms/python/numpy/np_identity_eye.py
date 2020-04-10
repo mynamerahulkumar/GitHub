affd8384d59ebad7b19e13fd4c48bf98ea8cea17
Rahul Kumar <mynamerahulkumar13@gmail.com>
2020-04-10 10:09:09 +0100

    removed algorithms

--- Algorithms/python/numpy/np_identity_eye.py
+++
@@ -1,9 +0,0 @@
-import numpy as np
-
-a,b=map(int,input().split())
-ide_mat=np.eye(a,b,k=0)
-print(str(ide_mat).replace('1',' 1').replace('0',' 0'))
-
-
-
-
--- Algorithms/python/numpy/numpy_concatenation.py
+++
@@ -1,8 +0,0 @@
-import numpy as np
-
-n,m,p=map(int,input().split())
-arrA=np.array([input().split() for _ in range(n)],int)
-arrB=np.array([input().split() for _ in range(m)],int )
-print(np.concatenate((arrA,arrB),axis=0))
-
-
