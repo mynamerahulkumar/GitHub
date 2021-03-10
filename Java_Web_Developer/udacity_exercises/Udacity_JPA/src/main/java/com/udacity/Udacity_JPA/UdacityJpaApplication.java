768799fa14ec647609fb45f521028c5bdc7d8bc2
Rahul Kumar <mynamerahulkumar13@gmail.com>
2021-03-10 14:44:38 +0000

    spring security updated

---
+++ Java_Web_Developer/udacity_exercises/Udacity_JPA/src/main/java/com/udacity/Udacity_JPA/UdacityJpaApplication.java
@@ -0,0 +1,13 @@
+package com.udacity.Udacity_JPA;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UdacityJpaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UdacityJpaApplication.class, args);
+	}
+
+}
---
+++ Java_Web_Developer/udacity_exercises/Udacity_JPA/src/test/java/com/udacity/Udacity_JPA/UdacityJpaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.udacity.Udacity_JPA;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UdacityJpaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
