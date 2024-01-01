b38b63cb6b8cb17e260d10735de681529d9d1917
unknown <mynamerahulkumar13@gmail.com>
2024-01-01 18:48:36 +0530

    added react parcel

--- Azure_devops_springboot/React-Javascript-Project/React/App.js
+++ Azure_devops_springboot/React-Javascript-Project/React/App.js
@@ -1,3 +1,5 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
 const heading=React.createElement("h1",{id:"heading"},
 "Hello world from React!");// tag,object,values// react first library
 //attributes to ta get {}
@@ -42,7 +44,7 @@ const parent1=React.createElement(
     "div",
     {id:"parent"},[
     React.createElement("div",{id:"child"},[
-    React.createElement("h1",{},"I am h1 tag"),
+    React.createElement("h1",{},"I am h1 tag2"),
     React.createElement("h2",{},"I am h2 tag"),
     ]),
     React.createElement("div",{id:"child2"},[
