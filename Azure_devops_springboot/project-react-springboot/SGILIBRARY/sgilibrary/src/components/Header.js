8cd033cc0183560b0a2421260af31d5f2aaf2ed7
unknown <mynamerahulkumar13@gmail.com>
2024-02-08 02:45:33 +0530

    react library

---
+++ Azure_devops_springboot/project-react-springboot/SGILIBRARY/sgilibrary/src/components/Header.js
@@ -0,0 +1,25 @@
+import SubHeader from "./SubHeader";
+
+
+const Header=()=>{
+
+    return(
+        <div>
+        <div className='flex bg-blue-100 shadow-lg flex-wrap'>
+            <img alt='sgi-logo' 
+              className='w-40 h-15 '
+             src='https://www.sokaglobal.org/wp-content/themes/soka/assets/img/logo01_pc.png'
+             />
+           
+            <img alt='lib-logo'
+            className='w-40 h-20 px-10'
+            src='https://lh3.googleusercontent.com/proxy/xZNSlUTPEUlNn-G9RZS1_Q9VYxFThhtFbdGawWO1MSemaKmaUoIzHMp9y4uLIvLktPB5wPvReyzdg8kL3O1Kv_prGZUDgIds-9SzXv9DknPKVcnxzhetIdx6hIHS98p9kJ6G'
+            />
+        </div>
+       
+        <SubHeader/>
+        </div>
+    )
+}
+
+export default Header;
---
+++ Azure_devops_springboot/project-react-springboot/SGILIBRARY/sgilibrary/src/components/SubHeader.js
@@ -0,0 +1,15 @@
+
+const SubHeader=()=>{
+    return (
+        <div className='flex bg-yellow-50 shadow-lg'>
+           <ul className='flex'>
+            <li className='p-2 px-10 font-bold '>NHR</li>
+            <li className='p-2 px-10  font-bold'>GOSHO</li>
+            <li className='p-2 px-10 font-bold'>Sensei Guidance</li>
+            <li className='p-2 px-10 font-bold'>Guidance for Youth</li>
+           </ul >
+        </div>
+    )
+}
+
+export default SubHeader;
